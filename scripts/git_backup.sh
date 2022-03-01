02ce13e280514c027443c2de28244e8bc1669e95
Rahul Vij <rahul.vij@gmail.com>
2022-03-01 10:31:25 -0800

    Backup triggered on 2022-03-01 10:31:25
    Klipper on commit: 8b0c6fc
    
    Moonraker on commit: f79439a
    
    Mainsail version: v2.1.2

--- scripts/git_backup.sh
+++ scripts/git_backup.sh
@@ -66,7 +66,7 @@ push_config(){
   git add . -v
   current_date=$(date +"%Y-%m-%d %T")
   git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
-  git push -u origin master
+  git push "git@ggithub.com:rahulvij/2.4-v900.git"
 }
 
 grab_version
